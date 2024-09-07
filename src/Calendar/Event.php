246efb9dad6d91f17749521aee2afdb8feb3f257
Nathan <nbish11@hotmail.com>
2024-09-07 05:11:56 +0000

    Provide a better abstraction of a calendar event

--- src/Calendar/Event.php
+++ src/Calendar/Event.php
@@ -9,16 +9,51 @@ use Brick\DateTime\LocalTime;
 use Brick\DateTime\ZonedDateTime;
 
 /**
- * @property-read string $self This is the URL to the event.
+ * Represents an event in a calendar.
+ *
+ * @property-read string $self This is the URL or identifier to the event.
+ * @property-read string $title The title of the event.
+ * @property-read ZonedDateTime $when The date and time the event starts and the time zone it is in.
+ * @property-read Duration $duration How long the event goes for
+ * @property-read string $organiser The person or group that organised the event. They do not have to be attending.
+ * @property-read string $location The location of the event. Can be a physical address, zoom link, etc.
+ * @property-read string $description More detailed information about the event.
+ * @property-read object[] $attendees A list of people who are attending the event.
  */
 class Event
 {
+	public string $location = '';
+	public string $description = '';
+	public array $attendees = [];
+
 	public function __construct(
 		public string $self,
 		public string $title,
 		public ZonedDateTime $when,
 		public Duration $duration,
+		public ?object $organiser = null,
 	) {
+	}
+
+	public function describe(string $description): self
+	{
+		$this->description = $description;
+
+		return $this;
+	}
+
+	public function attendAt(string $location): self
+	{
+		$this->location = $location;
+
+		return $this;
+	}
+
+	public function invite(object ...$attendees): self
+	{
+		$this->attendees = array_merge($this->attendees, $attendees);
+
+		return $this;
 	}
 
 	public function occursBetween(ZonedDateTime $from, ZonedDateTime $to): bool
